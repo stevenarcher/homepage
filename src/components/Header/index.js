9d7c269980e2f5d2363c869c227c1e9943aa412f
Steven Archer <email@stevenarcher.com>
2017-08-18 22:58:21 +0100

    adding test WebGL render to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,16 +6,55 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper, Title } from './styles';
+import * as material from '../../threejs/Materials';
+import * as THREE from 'three';
 import Renderer from '../../threejs/Renderer';
+import UpdateLoop from '../../utils/UpdateLoop';
 
 class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			width: 200,
+			height: 200,
+		};
+		this.sphere = new THREE.SphereGeometry(0.25, 8, 8);
+		this.mesh = new THREE.Mesh(this.sphere, material.plastic);
+
+		this.light = new THREE.DirectionalLight(0xffedc3, 0.6);
+		this.light.position.set(0, 2, 1);
+	}
+
+	componentDidMount() {
+		this.setState({ width: this.wrapper.width });
+		UpdateLoop.add(this);
+	}
+
+	componentWillUnmount() {
+		UpdateLoop.remove(this);
+	}
+
+	/**
+	 * Update
+	 * @param {number} delta - the time since the last update
+	 */
+	update = delta => {
+		this.mesh.position.z += 0.005;
+	};
+
 	render() {
 		return (
-			<Wrapper>
+			<Wrapper
+				ref={element => {
+					this.wrapper = element;
+				}}
+			>
+				<Renderer width={500} height={90}>
+					{[this.mesh, this.light]}
+				</Renderer>
 				<Title>
 					<span>Steven Archer</span>
 				</Title>
-				<Renderer width={200} height={200} />
 			</Wrapper>
 		);
 	}
