507855d8c2ffb032e1d2601c67ed53cbd7a224c6
Steven Archer <email@stevenarcher.com>
2017-08-27 20:42:28 +0100

    adding placeholder elements

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -16,7 +16,27 @@ export class App extends PureComponent {
 	render() {
 		return (
 			<div>
-				<Header />
+				<Header/>
+				<Section>
+					<h1>About</h1>
+					<span>brief description</span>
+				</Section>
+				<Section>
+					<h1>Projects</h1>
+					<span>This is where the project will be placed</span>
+				</Section>
+				<Section>
+					<h1>History</h1>
+					<span>D3 ... get from linked in?</span>
+				</Section>
+				<Section>
+					<h1>Photography </h1>
+					<span>flicker? instergram?</span>
+				</Section>
+				<Section>
+					<h1>Contact</h1>
+					<Social/>
+				</Section>
 			</div>
 		);
 	}
