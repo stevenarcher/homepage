b906c19f5e708575cf475edca1464cb97551c466
Steven Archer <email@stevenarcher.com>
2025-06-09 10:14:24 +0100

    updating astro and adding prettier

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/blog/model-vehicle-movement.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmodel-vehicle-movement.mdx&astroContentModuleFlag=true")]]);
+		
--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import remarkToc from 'remark-toc';
+import remarkCollapse from 'remark-collapse';
+import sitemap from '@astrojs/sitemap';
+import { SITE } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  alias: {
+    '@components': './src/components',
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
     sitemap(),
+    mdx(),
   ],
   markdown: {
     remarkPlugins: [
@@ -22,19 +27,19 @@ export default defineConfig({
       [
         remarkCollapse,
         {
-          test: "Table of contents",
+          test: 'Table of contents',
         },
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: 'one-dark-pro',
       wrap: true,
     },
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
+      exclude: ['@resvg/resvg-js'],
     },
   },
-  scopedStyleStrategy: "where",
+  scopedStyleStrategy: 'where',
 });
